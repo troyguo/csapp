29d13d2e939f0fb7414267e50f32948a442a7d4b
trogyuo <hit_troy@126.com>
2020-05-10 15:08:50 +0800

    2.55: update show_bytes to show long/string, add prefix before it to get better viewer

--- solution/ch2/show-bytes.c
+++ solution/ch2/show-bytes.c
@@ -3,14 +3,14 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 
 typedef unsigned char* byte_pointer;
 
 void show_bytes(byte_pointer start, size_t len)
 {
-	size_t i;
-	for ( i = 0; i < len; i++ ) {
+	for (size_t i = 0; i < len; i++ ) {
 		printf(" %.2x", start[i]);
 	}
 
@@ -20,38 +20,49 @@ void show_bytes(byte_pointer start, size_t len)
 
 void show_int(int x)
 {
+	printf("int: ");
 	show_bytes((byte_pointer)&x, sizeof(int));
 }
 
+void show_long(long x)
+{
+	printf("long: ");
+	show_bytes((byte_pointer)&x, sizeof(long));
+}
 
 void show_float(float x)
 {
-	show_bytes((byte_pointer)&x, sizeof(int));
+	printf("float: ");
+	show_bytes((byte_pointer)&x, sizeof(float));
 }
 
 void show_pointer(void *x)
 {
-	show_bytes((byte_pointer)&x, sizeof(int));
+	printf("pointer: ");
+	show_bytes((byte_pointer)&x, sizeof(void *));
 }
 
 void test_show_bytes (int val)
 {
 	int ival = val;
-
 	float fval = (float) ival;
 	int *pval = &ival;
+	const char *s = "abcdef";
+	long lval = (long)val;
 
 	show_int(ival);
 	show_float(fval);
+	show_long(lval);
 	show_pointer(pval);
 
-
+	printf("string: ");
+	show_bytes((byte_pointer) s , strlen(s));
 }
 
 int main(int argc, char *argv[])
 {
 
-	int test_num = 328;
+	int test_num = 328; //0x148
 
 	test_show_bytes(test_num);
 
